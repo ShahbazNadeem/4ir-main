5ff261b09a53395b176fef2c9fc6a2d4fb711218
shahbaz <shahbaznadeem654@gmail.com>
2025-01-03 12:33:50 +0500

    horizontal navbar done

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -1,8 +1,11 @@
-import React, { useState } from 'react'
+import React from 'react'
 import "../widgets.css"
 import Image from 'next/image'
 import Logo from "@/images/Logo.png"
 import { navbarData } from '@/data/layoutData'
+import { TiThMenu } from "react-icons/ti";
+import { IoIosMenu } from "react-icons/io";
+
 
 const Header = () => {
 
@@ -11,6 +14,12 @@ const Header = () => {
       <div className="wrapper-Header">
         <div className="container">
           <div className="header-content">
+
+            <div className="offcanvas-button">
+              <button><IoIosMenu size={30}/></button>
+            </div>
+
+
 
             <div className="header-logo">
               <figure>
@@ -25,7 +34,7 @@ const Header = () => {
                     {item.list && item.list.length > 0 ? (
                       <a
                         href={item.link}
-                        className="btn btn-secondary dropdown-toggle"
+                        className="simple-text dropdown-toggle"
                         role="button"
                         data-bs-toggle="dropdown"
                         aria-expanded="false"
@@ -57,14 +66,12 @@ const Header = () => {
               <button className='btn1'>Book A Free Demo</button>
             </div>
 
-            <div className="offcanvas-button">
-              <button>offcanvas</button>
-            </div>
+
           </div>
 
 
 
-          <div className="header-canves"></div>
+          <div className="header-canvas"></div>
         </div>
       </div>
     </section>
