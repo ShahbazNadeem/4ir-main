75cac520d4923546f8e16926d50d9fca4a33e94c
shahbaz <shahbaznadeem654@gmail.com>
2025-01-06 19:54:25 +0500

    footer completed

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -1,5 +1,9 @@
 import React from 'react'
 import Logo from "@/images/Logo.png"
+import Facebook from "@/svg/Facebook.png"
+import Instagram from "@/svg/Instagram.png"
+import Linkedin from "@/svg/Linkedin.png"
+import Twitter from "@/svg/Twitter.png"
 import Image from 'next/image'
 import { navbarData } from '@/data/layoutData'
 import { contactUsData } from '@/data/data'
@@ -9,7 +13,7 @@ const Footer = () => {
   return (
     <div>
       <footer>
-        {/* <section>
+        <section>
           <div className="wrapper-footer">
             <div className="container">
               <div className="footer-main">
@@ -23,63 +27,62 @@ const Footer = () => {
                   </div>
                 </div>
 
-
                 <div className="footer-2Section">
-                <div className="footer-sub-div">
-                  <div className="page-links">
-                    <h5>Company</h5>
-                    <ul>
-                      {navbarData?.slice(0, 5).map((item, index) => (
-                        <li key={index}>
-                          <p>{item.title}</p>
-                        </li>
-                      ))}
-                    </ul>
-                  </div>
-                </div>
-                <div className="footer-sub-div">
-                  <div className="resources">
-                  <h5>Resources</h5>
-                    <ul>
-                      <li><p>Privacy Policy</p></li>
-                      <li><p>Rate us</p></li>
-                      <li><p>Terms & Conditions</p></li>
-                    </ul>
-                  </div>
-                </div>
-                <div className="footer-sub-div">
-                  <div className="contact-info">
-                    <h5>Contact Info</h5>
-                    <ul>
-                      {
-                        contactUsData?.reverse().map((item, index) => (
+                  <div className="footer-sub-div">
+                    <div className="page-links">
+                      <h5>Company</h5>
+                      <ul>
+                        {navbarData?.slice(0, 5).map((item, index) => (
                           <li key={index}>
                             <p>{item.title}</p>
-                            <p>{item.title2}</p>
                           </li>
-                        ))
-                      }
-                    </ul>
+                        ))}
+                      </ul>
+                    </div>
                   </div>
-                </div>
+                  <div className="footer-sub-div">
+                    <div className="resources">
+                      <h5>Resources</h5>
+                      <ul>
+                        <li><p>Privacy Policy</p></li>
+                        <li><p>Rate us</p></li>
+                        <li><p>Terms & Conditions</p></li>
+                      </ul>
+                    </div>
+                  </div>
+                  <div className="footer-sub-div">
+                    <div className="contact-info">
+                      <h5>Contact Info</h5>
+                      <ul>
+                        {
+                          contactUsData?.reverse().map((item, index) => (
+                            <li key={index}>
+                              <p>{item.title}</p>
+                              <p>{item.title2}</p>
+                            </li>
+                          ))
+                        }
+                      </ul>
+                    </div>
+                  </div>
                 </div>
               </div>
               <div className="footer-bottom">
                 <div className="footer-copyright">
-                © 4IR @ 2024 All the Copyright Reserved. 
+                  © 4IR @ 2024 All the Copyright Reserved.
                 </div>
                 <div className="footer-social-links">
                   <ul>
-                    <li><a href="#"><i className="fab fa-facebook-f"></i></a></li>
-                    <li><a href="#"><i className="fab fa-twitter"></i></a></li>
-                    <li><a href="#"><i className="fab fa-linkedin-in"></i></a></li>
-                    <li><a href="#"><i className="fab fa-instagram"></i></a></li>
+                    <li><a href="#"><Image src={Facebook} /></a></li>
+                    <li><a href="#"><Image src={Instagram} /></a></li>
+                    <li><a href="#"><Image src={Linkedin} /></a></li>
+                    <li><a href="#"><Image src={Twitter} /></a></li>
                   </ul>
                 </div>
               </div>
             </div>
           </div>
-        </section> */}
+        </section>
       </footer>
     </div>
   )
